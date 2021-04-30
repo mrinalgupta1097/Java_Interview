be598aec41832a6de95d4495d3df589b2b0ce3fb
mrinal gupta <mrinalgupta1097@gmail.com>
2021-04-30 08:28:00 +0530

    Tree Insertion Non Recursive Solution Added

--- src/com/mrinalgupta/trees/TreeInsertionRecursiveSolution.java
+++ src/com/mrinalgupta/trees/TreeInsertionRecursiveSolution.java
@@ -1,4 +1,16 @@
 package com.mrinalgupta.trees;
 
 public class TreeInsertionRecursiveSolution {
+    public TreeNode insertIntoBST(TreeNode root,int value){
+        if(root == null){
+            return new TreeNode(value);
+        }
+        if(root.value > value){
+            root.left = insertIntoBST(root.left, value);
+        }else{
+            root.right = insertIntoBST(root.right, value);
+        }
+        return root;
+    }
+
 }
